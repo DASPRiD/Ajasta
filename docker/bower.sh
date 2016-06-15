abf3db811942def34d8e326e59fd723896a3e742
Ben Scholzen <mail@dasprids.de>
2016-06-15 18:39:24 +0200

    Add missing parameters in bower.sh

--- docker/bower.sh
+++ docker/bower.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker-compose up -d application
-docker exec -it $(docker-compose ps -q application | head -1) bower --allow-root
+docker exec -it $(docker-compose ps -q application | head -1) bower --allow-root $@
 docker exec -it $(docker-compose ps -q application | head -1) chown -R $(id -u):$(id -g) vendor
